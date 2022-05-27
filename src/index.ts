30ef206795c818779826b9ed905719b7694776ff
Dev_sammy <yinkaadebayo31@gmail.com>
2022-05-27 15:54:19 +0100

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+const todoValue:any = document.querySelector('#todo-add')
+const btn:any = document.querySelector('.add-btn')
+const ul:any = document.querySelector('.todo-container')
+
+function todoAction(){
+    btn.addEventListener('click', addTodo)
+    ul.addEventListener('click', removeTodo)
+    ul.addEventListener('click', checkTodo)
+    function addTodo(){
+         
+            //Create todo container
+            const div = document.createElement('div')
+            div.classList.add('todo-items')
+            
+            
+            //Create todo items
+            const li = document.createElement('li')
+            li.classList.add('todo-item')
+            li.innerText = todoValue.value
+            
+            
+            //Check Button
+            const checkBtn = document.createElement('i')
+            checkBtn.classList.add('fas')
+            checkBtn.classList.add('fa-check')
+            checkBtn.classList.add('checked')
+            
+            
+            //Delete Button
+            const delBtn = document.createElement('i')
+            delBtn.classList.add('fas')
+            delBtn.classList.add('fa-trash')
+            delBtn.classList.add('delete')
+            
+    
+            //Adding todo items to the container    
+            div.appendChild(li)
+            div.appendChild(checkBtn)
+            div.appendChild(delBtn)
+    
+            ul.appendChild(div)
+    
+            todoValue.value = ""
+    }
+}
+
+function removeTodo(e){
+
+    const item = e.target
+
+    if(item.classList[2]==='delete'){
+        const todo = item.parentElement
+        todo.remove()
+    }
+
+}
+
+function checkTodo(e){
+
+    const item = e.target
+
+    if(item.classList[2]==='checked'){
+        const todo = item.parentElement
+        todo.classList.toggle('line')
+    }
+
+}
+
+
+
+todoAction()
